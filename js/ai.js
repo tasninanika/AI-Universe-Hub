474e4ffb7f4cac47c2930e1467042576cf560fe4
anikatasnin <jarintasnin09@gmail.com>
2024-09-08 01:32:17 +0000

    get single ai

--- js/ai.js
+++ js/ai.js
@@ -22,12 +22,35 @@ const displayAi = (ai) =>{
         console.log(singleAi);
 
          // create div
-    const div = document.createElement('div');
+        const div = document.createElement('div');
 
-    // set inner html in div
-    div.innerHTML = `
+        // added classes on div
+        div.className = `card border-2 w-80`;
 
-    `;
+        // set inner html in div
+        div.innerHTML = `
+            <div class="p-5">
+            <figure class="p-10 rounded-xl bg-[#0D6EFD0D]">
+              <img src="${singleAi.image}" alt="phone" class="rounded-xl" />
+            </figure>
+        </div>
+        <div class="card-body items-center text-center">
+              <h2 class="card-title text-2xl font-semibold mb-3">Features</h2>
+              <ol class="text-sm text-[#706F6F]">
+              <li>${singleAi?.features[0]}</li>
+              <li>${singleAi?.features[1]}</li>
+              <li>${singleAi?.features[2]}</li>
+              </ol>
+              <h3 class="text-2xl font-bold pb-2">$999</h3>
+              <div class="card-actions">
+                <button onclick="showDetails('')" class="btn bg-[#74700e] text-white">Show Details</button>
+              </div>
+        </div>
+        `;
+
+        // append div on container
+        aiContainer.appendChild(div);
+        console.log(div);
     });
 
    
