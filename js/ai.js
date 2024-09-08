fbd6ab6f2303d67ba238c4a7eb91f425baef26b5
anikatasnin <jarintasnin09@gmail.com>
2024-09-08 12:33:17 +0000

    single card added

--- js/ai.js
+++ js/ai.js
@@ -25,13 +25,13 @@ const displayAi = (ai) =>{
         const div = document.createElement('div');
 
         // added classes on div
-        div.className = `card border-2 w-80`;
+        div.className = `card border-2 w-96`;
 
         // set inner html in div
         div.innerHTML = `
             <div class="pt-5 pl-5 pr-5">
             <figure class=" rounded-xl bg-[#0D6EFD0D]">
-              <img src="${singleAi.image}" alt="phone" class="rounded-xl" />
+              <img src="${singleAi.image}" alt="ai" class="rounded-xl" />
             </figure>
         </div>
         <div class="card-body">
@@ -42,9 +42,16 @@ const displayAi = (ai) =>{
               <li>3. ${singleAi?.features[2]}</li>
               </ol>
               
-              <h3 class="text-2xl font-bold pb-2">$999</h3>
-              <div class="card-actions">
-                <button onclick="showDetails('')" class="btn bg-[#74700e] text-white">Show Details</button>
+              <div class="flex">
+                <div class="">
+                  <h3 class="text-2xl font-bold pb-4">${singleAi?.name}</h3>
+              <p class="flex gap-1 text-sm text-[#706F6F]"><img src="images/Frame.png" alt="" class="h-[20px] w-[20px] mb-1">${singleAi?.published_in}</p>
+              </div>
+              <div class="card-actions justify-end flex-1 mt-5">
+                <button onclick="showDetails('')" class="btn bg-[#FEF7F7] text-white rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none">
+                <path d="M4.5 12H19.5M19.5 12L12.75 5.25M19.5 12L12.75 18.75" stroke="#EB5757" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg></button>
+              </div>
               </div>
         </div>
         `;
