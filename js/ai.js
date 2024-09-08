fd93fe15d1921f638d3d36050cdc33cce6c97dc3
anikatasnin <jarintasnin09@gmail.com>
2024-09-08 00:42:54 +0000

    get ai container

--- js/ai.js
+++ js/ai.js
@@ -1,8 +1,19 @@
-const aiUniverse = async () =>{
+// fetch data
+const loadAiUniverse = async () =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
     const data = await res.json();
     const ai = data.data;
     console.log(data);
     console.log(ai);
+    displayAi(ai);
 }
-aiUniverse();
+loadAiUniverse();
+
+// single card
+const displayAi = (ai) =>{
+    console.log(ai);
+
+    // card
+    const aiContainer = document.getElementById('ai-container');
+    console.log(aiContainer);
+}
