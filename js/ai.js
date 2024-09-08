9db56cba810a27cf826ad7f4461ecdf20b561e62
anikatasnin <jarintasnin09@gmail.com>
2024-09-08 01:07:25 +0000

    get single ai

--- js/ai.js
+++ js/ai.js
@@ -2,19 +2,36 @@
 const loadAiUniverse = async () =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
     const data = await res.json();
-    const ai = data.data;
+    const ai = data.data.tools;
     console.log(data);
     console.log(ai);
     displayAi(ai);
 }
-loadAiUniverse();
+
 
 // single card
 const displayAi = (ai) =>{
     console.log(ai);
 
-    // card
+    // get the container
     const aiContainer = document.getElementById('ai-container');
     console.log(aiContainer);
-    
+
+    // for each card
+    ai.forEach(singleAi => {
+        console.log(singleAi);
+
+         // create div
+    const div = document.createElement('div');
+
+    // set inner html in div
+    div.innerHTML = `
+
+    `;
+    });
+
+   
+
 }
+
+loadAiUniverse();
